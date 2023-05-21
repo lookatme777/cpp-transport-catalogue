1c1e371ba3bd4382f2f61595b18d4ce8e18e1db0
camuSssssssssssssssssssssssssssssssssss <83710316+camuSssssssssssssssssssssssssssssssssss@users.noreply.github.com>
2023-05-21 18:41:35 +0400

    Update input_reader.h

--- transport-catalogue/input_reader.h
+++ transport-catalogue/input_reader.h
@@ -1,1 +1,16 @@
-// место для вашего кода
+#pragma once
+
+#include "geo.h"
+#include "transport_catalogue.h"
+
+#include <iostream>
+
+/*input_reader.h, input_reader.cpp — чтение запросов на заполнение базы;*/
+
+namespace fill {
+
+	void TransportCatalogue(infocatalogueclass::TransportCatalogue&);
+	infostruct::Bus ParseBus(std::string&);
+	infostruct::Stop ParseStop(std::string&);
+	void AddStopDistances(std::string&, infocatalogueclass::TransportCatalogue&);
+}
