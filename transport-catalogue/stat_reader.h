0c552b9bf59db9679b8e2762796659135a91e0d8
camuSssssssssssssssssssssssssssssssssss <83710316+camuSssssssssssssssssssssssssssssssssss@users.noreply.github.com>
2023-05-21 18:44:08 +0400

    Update stat_reader.h

--- transport-catalogue/stat_reader.h
+++ transport-catalogue/stat_reader.h
@@ -1,1 +1,9 @@
-// место для вашего кода
+#pragma once
+#include "transport_catalogue.h"
+#include <iostream>
+/*stat_reader.h, stat_reader.cpp — чтение запросов на вывод и сам вывод;*/
+namespace out {
+	void Transport(infocatalogueclass::TransportCatalogue& catalogue);
+	void Bus(infocatalogueclass::TransportCatalogue&, std::string);
+	void Stop(infocatalogueclass::TransportCatalogue&, std::string);
+}
