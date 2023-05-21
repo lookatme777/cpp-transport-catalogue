23a2e7b885bb5ac9355ff3c518f0b86e8c6fc961
camuSssssssssssssssssssssssssssssssssss <83710316+camuSssssssssssssssssssssssssssssssssss@users.noreply.github.com>
2023-05-21 18:41:46 +0400

    Update main.cpp

--- transport-catalogue/main.cpp
+++ transport-catalogue/main.cpp
@@ -1,1 +1,9 @@
-// место для вашего кода
+#include "input_reader.h"
+#include "stat_reader.h"
+
+// main.cpp — точка входа.
+int main() {
+    infocatalogueclass::TransportCatalogue catalogue;
+    fill::TransportCatalogue(catalogue);
+    out::Transport(catalogue);
+}
