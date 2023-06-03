a6359c70ee55bef7356ecfd7e867f9269ab085fa
camuSssssssssssssssssssssssssssssssssss <83710316+camuSssssssssssssssssssssssssssssssssss@users.noreply.github.com>
2023-06-03 17:27:46 +0400

    Delete stat_reader.h

--- transport-catalogue/stat_reader.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-#include "transport_catalogue.h"
-#include <iostream>
-/*stat_reader.h, stat_reader.cpp — чтение запросов на вывод и сам вывод;*/
-namespace out {
-	void Transport(infocatalogueclass::TransportCatalogue&, std::istream&);
-	void Bus(infocatalogueclass::TransportCatalogue&, const std::string&, std::ostream&);
-	void Stop(infocatalogueclass::TransportCatalogue&, const std::string&, std::ostream&);
-}
