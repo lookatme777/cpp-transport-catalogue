6c3b14684810db32f6b9ee1a54dd113b768bb087
camuSssssssssssssssssssssssssssssssssss <83710316+camuSssssssssssssssssssssssssssssssssss@users.noreply.github.com>
2023-06-03 21:33:15 +0400

    Update json_reader.h

--- transport-catalogue/json_reader.h
+++ transport-catalogue/json_reader.h
@@ -7,20 +7,18 @@
 
 #include <iostream>
 
-class jReader {
+class JSONReader {
 public:
-    jReader(std::istream& input): input_(json::Load(input)){
+    JSONReader(std::istream& input): input_(json::Load(input)){
     }
 
     const json::Node& RenderSettings() const;
     const json::Node& BaseRequests() const;
     const json::Node& StatRequests() const;
-    void ProcessRequests(const json::Node& stat_requests, RequestHandler& rh) const;
+   
     void FillCatalogue(infocatalogue::TransportCatalogue&);
     renderer::MapRenderer FillRenderSettings(const json::Dict& request_map) const;
-    const json::Node PrintStop(const json::Dict&, RequestHandler&) const;
-    const json::Node PrintRoute(const json::Dict&, RequestHandler&) const;
-    const json::Node PrintMap(const json::Dict&, RequestHandler&) const;
+
 
 private:
     json::Document input_;
