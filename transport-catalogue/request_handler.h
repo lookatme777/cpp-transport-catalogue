5c391b1db4a898dc3a224d7d80fd6fc1b76c0a35
camuSssssssssssssssssssssssssssssssssss <83710316+camuSssssssssssssssssssssssssssssssssss@users.noreply.github.com>
2023-06-10 17:11:36 +0400

    Update request_handler.h

--- transport-catalogue/request_handler.h
+++ transport-catalogue/request_handler.h
@@ -3,24 +3,24 @@
 #include "json.h"
 #include "transport_catalogue.h"
 #include "map_renderer.h"
-#include "json_reader.h"
 
 #include <sstream>
 
 class RequestHandler {
 public:
-    RequestHandler(const infocatalogue::TransportCatalogue& catalogue, const renderer::MapRenderer& renderer): catalogue_(catalogue), renderer_(renderer){
-    }
+    RequestHandler(const infocatalogue::TransportCatalogue& catalogue, const renderer::MapRenderer& renderer): catalogue_(catalogue), renderer_(renderer)
+    {}
 
-    std::optional<domain::RouteInforamtion> GetBusStat(const std::string_view) const;
-    const std::set<std::string> GetBusesByStop(std::string_view) const;
-    bool IsBusNumber(const std::string_view) const;
-    bool IsStopName(const std::string_view) const;
+    std::optional<domain::RouteInforamtion> GetBusStat(const std::string_view bus_number) const;
+    const std::set<std::string> GetBusesByStop(std::string_view stop_name) const;
+    bool IsBusNumber(const std::string_view bus_number) const;
+    bool IsStopName(const std::string_view stop_name) const;
 
     void ProcessRequests(const json::Node& stat_requests) const;
-    const json::Node PrintStop(const json::Dict&) const;
-    const json::Node PrintRoute(const json::Dict&) const;
-    const json::Node PrintMap(const json::Dict&) const;
+
+    const json::Node PrintRoute(const json::Dict& request_map) const;
+    const json::Node PrintStop(const json::Dict& request_map) const;
+    const json::Node PrintMap(const json::Dict& request_map) const;
 
     svg::Document RenderMap() const;
 
