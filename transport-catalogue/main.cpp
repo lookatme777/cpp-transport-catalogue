eb02b89dd23ca98e396bc61f9021bbc2ffede79d
camuSssssssssssssssssssssssssssssssssss <83710316+camuSssssssssssssssssssssssssssssssssss@users.noreply.github.com>
2023-07-17 15:01:26 +0400

    Update main.cpp

--- transport-catalogue/main.cpp
+++ transport-catalogue/main.cpp
@@ -2,16 +2,17 @@
 #include "request_handler.h"
 
 int main() {
-
     infocatalogue::TransportCatalogue catalogue;
     JSONReader doc(std::cin);
 
     json_doc.FillCatalogue(catalogue);
 
     const auto& stat_requests = doc.StatRequests();
-    const auto& render_settings = doc.RenderSettings().AsMap();
+    const auto& render_settings = doc.RenderSettings();
     const auto& renderer = doc.FillRenderSettings(render_settings);
+    const auto& routing_settings = doc.FillRoutingSettings(doc.RoutingSettings());
+    const transport_router::Router router = { routing_settings, catalogue };
 
-    RequestHandler rh(catalogue, renderer);
+    RequestHandler rh(catalogue, renderer, router);
     rh.ProcessRequests(stat_requests);
 }
