4f54df3773df0360e48f72a5bc69f1deb0e7d098
camuSssssssssssssssssssssssssssssssssss <83710316+camuSssssssssssssssssssssssssssssssssss@users.noreply.github.com>
2023-07-30 13:41:03 +0400

    Update domain.h

--- transport-catalogue/domain.h
+++ transport-catalogue/domain.h
@@ -1,37 +1,30 @@
 #pragma once
+
 #include "geo.h"
+
 #include <string>
-#include <vector>
-#include <set>
 #include <unordered_map>
+#include <vector>
+#include <string_view>
 
 namespace domain {
 
     struct Stop {
+        Stop(const std::string& name, const geo::Coordinates& coordinates);
+        int GetDistance(Stop* to);
+
         std::string name;
         geo::Coordinates coordinates;
-        std::set<std::string> buses_by_stop;
+        std::unordered_map<std::string_view, int> stop_distances;
     };
 
     struct Bus {
-        std::string number;
-        std::vector<const Stop*> stops;
+        Bus(const std::string& name, std::vector<Stop*> stops, bool is_circle);
+
+        std::string name;
+        std::vector<Stop*> stops;
         bool is_circle;
+        Stop* final_stop = nullptr;
     };
 
-    struct RouteInforamtion {
-        size_t stops_count;
-        size_t unique_stops_count;
-        double route_length;
-        double curvature;
-    };
-
-    struct Hasher {
-        size_t operator()(const std::pair<const Stop*, const Stop*>& points) const {
-            size_t hash_first = std::hash<const void*>{}(points.first);
-            size_t hash_second = std::hash<const void*>{}(points.second);
-            return hash_first + hash_second * 37;
-        }
-    };
-
-}
+} //namespace domain
