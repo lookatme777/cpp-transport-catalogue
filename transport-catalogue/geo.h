c881c02982c319e1e2ea1808cf2ff1803d84cf5e
camuSssssssssssssssssssssssssssssssssss <83710316+camuSssssssssssssssssssssssssssssssssss@users.noreply.github.com>
2023-07-30 13:41:50 +0400

    Update geo.h

--- transport-catalogue/geo.h
+++ transport-catalogue/geo.h
@@ -1,17 +1,10 @@
 #pragma once
 
-const int RADIUSEARTH = 6371000;
 namespace geo {
 
     struct Coordinates {
-        double lat;
-        double lng;
-        bool operator==(const Coordinates& other) const {
-            return lat == other.lat && lng == other.lng;
-        }
-        bool operator!=(const Coordinates& other) const {
-            return !(*this == other);
-        }
+        double lat; // Широта
+        double lng; // Долгота
     };
 
     double ComputeDistance(Coordinates from, Coordinates to);
