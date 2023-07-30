482c743e6e8a82021172a1d05af7681af18a56fe
camuSssssssssssssssssssssssssssssssssss <83710316+camuSssssssssssssssssssssssssssssssssss@users.noreply.github.com>
2023-07-30 13:46:10 +0400

    Update request_handler.h

--- transport-catalogue/request_handler.h
+++ transport-catalogue/request_handler.h
@@ -1,35 +1,32 @@
 #pragma once
 
-#include "json.h"
 #include "transport_catalogue.h"
-#include "map_renderer.h"
 #include "transport_router.h"
+#include "domain.h"
+#include "json.h"
+#include "map_renderer.h"
+#include "json_builder.h"
 
-#include <sstream>
+#include <utility>
+#include <string>
+#include <string_view>
 
 class RequestHandler {
 public:
-    RequestHandler(const infocatalogue::TransportCatalogue& catalogue, const renderer::MapRenderer& renderer, const transport_router::Router& router): catalogue_(catalogue), renderer_(renderer), router_(router)
-    {}
+    RequestHandler(const transport::Catalogue& catalogue,
+        const transport::Router& router, const renderer::MapRenderer& renderer);
 
-    std::optional<domain::RouteInforamtion> GetBusStat(const std::string_view bus_number) const;
-    const std::set<std::string> GetBusesByStop(std::string_view stop_name) const;
-    bool IsBusNumber(const std::string_view bus_number) const;
-    bool IsStopName(const std::string_view stop_name) const;
-    const std::optional<graph::Router<double>::RouteInfo> GetOptimalRoute(const std::string_view stop_from, const std::string_view stop_to) const;
-    const graph::DirectedWeightedGraph<double>& GetRouterGraph() const;
-
-    void ProcessRequests(const json::Node& stat_requests) const;
-
-    const json::Node PrintRoute(const json::Dict& request_map) const;
-    const json::Node PrintStop(const json::Dict& request_map) const;
-    const json::Node PrintMap(const json::Dict& request_map) const;
-    const json::Node PrintRouting(const json::Dict& request_map) const;
+    void JsonStatRequests(const json::Node& json_doc, std::ostream& output);
 
     svg::Document RenderMap() const;
 
 private:
-    const infocatalogue::TransportCatalogue& catalogue_;
+    const transport::Catalogue& db_;
+    const transport::Router& router_;
     const renderer::MapRenderer& renderer_;
-    const transport_router::Router& router_;
+
+    json::Node FindStopRequestProcessing(const json::Dict& request_map);
+    json::Node FindBusRequestProcessing(const json::Dict& request_map);
+    json::Node BuildMapRequestProcessing(const json::Dict& request_map);
+    json::Node BuildRouteRequestProcessing(const json::Dict& request_map);
 };
