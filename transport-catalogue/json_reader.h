c48fb0ee56d20be0706bdd2e33b9667fe0ad8d42
camuSssssssssssssssssssssssssssssssssss <83710316+camuSssssssssssssssssssssssssssssssssss@users.noreply.github.com>
2023-07-30 13:43:31 +0400

    Update json_reader.h

--- transport-catalogue/json_reader.h
+++ transport-catalogue/json_reader.h
@@ -2,32 +2,48 @@
 
 #include "json.h"
 #include "transport_catalogue.h"
-#include "map_renderer.h"
-#include "transport_router.h"
-#include "request_handler.h"
+#include "domain.h"
 
-#include <iostream>
+#include <unordered_map>
+#include <string>
+#include <string_view>
+#include <vector>
 
-class JSONReader {
+class JsonReader {
 public:
-    JSONReader(std::istream& input) : input_(json::Load(input)) {
-    }
+    JsonReader(json::Document input_json)
+        : input_(input_json) {}
 
-    const json::Node& RenderSettings() const;
-    const json::Node& BaseRequests() const;
-    const json::Node& StatRequests() const;
-    const json::Node& RoutingSettings() const;
+    const json::Node& GetBaseRequest() const;
 
-    void FillCatalogue(infocatalogue::TransportCatalogue&);
-    renderer::MapRenderer FillRenderSettings(const json::Node& settings) const;
-    transport_router::Router FillRoutingSettings(const json::Node& settings) const;
+    const json::Node& GetStatRequest() const;
 
+    const json::Node& GetRenderSettings() const;
+
+    const json::Node& GetRoutingSettings() const;
+
+    const json::Node& GetSerializationSettings() const;
+
+    void FillCatalogue(transport::Catalogue& catalogue) const;
 
 private:
     json::Document input_;
-    json::Node null_ = nullptr;
+    json::Node dumm_ = nullptr;
 
-    std::tuple<std::string_view, geo::Coordinates, std::map<std::string_view, int>> FillStop(const json::Dict&) const;
-    void FillStopDistances(infocatalogue::TransportCatalogue&) const;
-    std::tuple<std::string_view, std::vector<const domain::Stop*>, bool> FillRoute(const json::Dict&, infocatalogue::TransportCatalogue&) const;
+    struct Bus_info {
+        std::vector<std::string_view> stops;
+        std::string_view final_stop;
+        bool is_circle;
+    };
+
+    using StopsDistMap = std::unordered_map<std::string_view, std::unordered_map<std::string_view, int>>;
+    using BusesInfoMap = std::unordered_map<std::string_view, Bus_info>;
+
+    void ParseStopAddRequest(transport::Catalogue& catalogue, const json::Dict& request_map,
+        StopsDistMap& stop_to_stops_distance) const;
+    void SetStopsDistances(transport::Catalogue& catalogue,
+        const StopsDistMap& stop_to_stops_distance) const;
+    void ParseBusAddRequest(const json::Dict& request_map, BusesInfoMap& buses_info) const;
+    void BusesAddProcess(transport::Catalogue& catalogue, const BusesInfoMap& buses_info) const;
+    void SetFinals(transport::Catalogue& catalogue, const BusesInfoMap& buses_info) const;
 };
